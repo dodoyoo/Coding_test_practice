3dde7a7fada4c61f1578cde273fbdd6c353e03c6
dodoyoo <dong15903@daum.net>
2024-04-29 15:27:30 +0900

    feat 2024-04-29

--- string/2024-04/29.js
+++ string/2024-04/29.js
@@ -1,29 +1,26 @@
-//  조건 문자열
+// 문자열 여러 번 뒤집기
 
 /*
 문제
-문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
-
-* 두 수가 n과 m이라면
-    * ">", "=" : n >= m
-    * "<", "=" : n <= m
-    * ">", "!" : n > m
-    * "<", "!" : n < m
-
-두 문자열 ineq와 eq가 주어집니다.
-ineq는 “<“ 와 ”>” 중 하나고, eq는 “=“ 와 “!” 중 하나입니다.
-그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 
-return하도록 solution함수를 완성해 주세요.
+문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다.
+queries의 원소는 [s. e]형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다.
+my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
 */
 
-function solution(ineq, eq, n, m) {
-  if (eq === '=' && n === m) return 1;
-  if (ineq === '<' && n < m) return 1;
-  if (ineq === '>' && n > m) return 1;
-  return 0;
+function solution(my_string, queries) {
+  let str = my_string.split('');
+  queries.forEach(([start, end]) => {
+    const changeStr = str.slice(start, end + 1);
+    str.splice(start, changeStr.length, ...changeStr.reverse());
+  });
+  return str.join('');
 }
 
 /*
+고민
+- my_string을 쪼개주고 뒤집는 메서드를 이용해서 범위를 정해준다.
+
 해결
-- 문제에서 주어진 조건문을 활용해서 문제를 풀이하였다.
+- my_string을 쪼개준 뒤 forEach()를 사용해서 한번씩 출력하게 해준다
+- 그 이후 출력되어야 할 것들을 reverse를 해주고 마지막으로 join을 해줘서 문자열로 나오게 해준다.
 */
