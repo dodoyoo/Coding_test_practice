381b13387d042de6b81658280570b61ced0fac14
dodoyoo <dong15903@daum.net>
2024-05-14 12:22:23 +0900

    feat 2024-05-17

---
+++ code_kata_practice/Level 0/입문 문제/두수의 차.js
@@ -0,0 +1,14 @@
+// 두수의 차
+
+/*
+문제
+정수 num1과 num2가 주어질 때, num1에서 num2를 
+뺀 값을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(num1, num2) {
+  var answer = num1 - num2;
+  return answer;
+}
+
+console.log(solution(2, 3)); // -1
