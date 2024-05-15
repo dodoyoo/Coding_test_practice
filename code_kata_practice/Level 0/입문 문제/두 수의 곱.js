98caad09a2970f80c25e7dcdc814276bbfc1f4b5
dodoyoo <dong15903@daum.net>
2024-05-15 12:26:31 +0900

    feat 2024-05-15

---
+++ code_kata_practice/Level 0/입문 문제/두 수의 곱.js
@@ -0,0 +1,13 @@
+// 두 수의 곱
+
+/*
+문제
+정수 num1, num2가 매개변수 주어집니다.
+num1과 num2를 곱한 값을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(solution(2, 3)); // 6
