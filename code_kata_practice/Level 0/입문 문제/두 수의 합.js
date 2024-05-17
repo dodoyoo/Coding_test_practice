4776a0e50339519545deaba8c1014eebf75767d9
dodoyoo <dong15903@daum.net>
2024-05-17 12:38:36 +0900

    feat 2024-05-17(1)

---
+++ code_kata_practice/Level 0/입문 문제/두 수의 합.js
@@ -0,0 +1,13 @@
+// 두 수의 합
+
+/*
+문제
+정수 num1 과 num2가 주어질 때,
+num1과 num2의 합을 return하도록 solution 함수를 완성해 주세요.
+*/
+
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(solution(2, 3)); // 5
