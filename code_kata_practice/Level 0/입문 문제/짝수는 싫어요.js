c5ce4c1bb1e6d6c2b643d881f8b846acf468ede0
dodoyoo <dong15903@daum.net>
2024-05-31 00:27:20 +0900

    feat 2024-05-31

---
+++ code_kata_practice/Level 0/입문 문제/짝수는 싫어요.js
@@ -0,0 +1,17 @@
+// 짝수는 싫어요
+
+/* 
+문제
+정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 
+담긴 배열을 return 하도록  solution 함수를 완성해 주세요.
+*/
+
+function solution(n) {
+  let answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
