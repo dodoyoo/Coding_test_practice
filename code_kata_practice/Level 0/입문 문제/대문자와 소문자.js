47d0a66c55e808402cb81d66541ccfe72c219673
dodoyoo <dong15903@daum.net>
2024-06-13 10:18:08 +0900

    feat 2024-06-13

---
+++ code_kata_practice/Level 0/입문 문제/대문자와 소문자.js
@@ -0,0 +1,17 @@
+// 대문자와 소문자
+
+/*
+문제
+문자열 my_string이 매개변수로 주어질 때,
+대문자는 소문자로 소문자는 대문자로 변환한 문자열을 
+return하는 solution 함수를 완성해주세요.
+*/
+
+function solution(my_string) {
+  return [...my_string]
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join('');
+}
+
+console.log(solution('cccCCC')); // “CCCccc”
+console.log(solution('abCdEfghIJ')); //"ABcDeFGHij"
