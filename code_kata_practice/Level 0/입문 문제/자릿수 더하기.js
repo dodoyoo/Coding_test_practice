7e548abe0f56222eec53eea3fdffb348b8c729b7
dodoyoo <dong15903@daum.net>
2024-06-20 14:25:14 +0900

    feat 2024-06-20

---
+++ code_kata_practice/Level 0/입문 문제/자릿수 더하기.js
@@ -0,0 +1,19 @@
+// 자릿수 더하기
+
+/*
+문제
+정수 n이 매개변수로 주어질 때 
+n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(n) {
+  var answer = 0;
+  let result = n.toString().split('');
+  for (let i = 0; i < result.length; i++) {
+    answer += parseInt(result[i]);
+  }
+  return answer;
+}
+
+console.log(solution(1234)); // 10
+console.log(solution(930211)); // 16
