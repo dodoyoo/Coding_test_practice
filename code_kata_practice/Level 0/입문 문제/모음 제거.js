c530704aaea8105aad67182cfba1ecc125b4f26f
dodoyoo <dong15903@daum.net>
2024-08-20 11:30:18 +0900

    feat 2024-08-20

--- code_kata_practice/Level 0/입문 문제/모음 제거.js
+++ code_kata_practice/Level 0/입문 문제/모음 제거.js
@@ -13,3 +13,10 @@ function solution(my_string) {
 
 console.log(solution('bus')); // bs
 console.log(solution('nice to meet you')); // nc t mt y
+
+// case 2
+function solution(my_string) {
+  return Array.from(my_string)
+    .filter((t) => !['a', 'e', 'i', 'o', 'u'].includes(t))
+    .join('');
+}
