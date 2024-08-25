b55e84daf32356fef04a77255f43728a63167c2f
dodoyoo <dong15903@daum.net>
2024-08-25 13:04:59 +0900

    feat 2024-08-25

--- code_kata_practice/Level 0/입문 문제/배열의 평균값.js
+++ code_kata_practice/Level 0/입문 문제/배열의 평균값.js
@@ -16,3 +16,9 @@ function solution(numbers) {
 
 console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 5.5
 console.log(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99])); // 94.0
+
+// case 2
+function solution(numbers) {
+  var answer = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return answer;
+}
