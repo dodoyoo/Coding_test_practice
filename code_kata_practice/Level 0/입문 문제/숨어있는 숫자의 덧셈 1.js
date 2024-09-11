76c66254669baa434626dd4551abbb66b9f19bb7
dodoyoo <dong15903@daum.net>
2024-09-11 23:15:14 +0900

    feat 2024-09-11

--- code_kata_practice/Level 0/입문 문제/숨어있는 숫자의 덧셈 1.js
+++ code_kata_practice/Level 0/입문 문제/숨어있는 숫자의 덧셈 1.js
@@ -15,3 +15,12 @@ function solution(my_string) {
 
 console.log(solution('aAb1B2cC34oOp')); // 10
 console.log(solution('1a2b3c4d123')); // 16
+
+// case 2
+function solution(my_string) {
+  const answer = my_string
+    .replace(/[^0-9]/g, '')
+    .split('')
+    .reduce((acc, curr) => acc + Number(curr), 0);
+  return answer;
+}
