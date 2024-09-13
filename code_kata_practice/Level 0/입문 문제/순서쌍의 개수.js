11b21e460c110c5bd71755ee74ebabf84ac099ad
dodoyoo <dong15903@daum.net>
2024-09-13 23:12:47 +0900

    feat 2024-09-13

--- code_kata_practice/Level 0/입문 문제/순서쌍의 개수.js
+++ code_kata_practice/Level 0/입문 문제/순서쌍의 개수.js
@@ -22,3 +22,11 @@ console.log(solution(20));
 // 6  => (1, 20), (2, 10), (4, 5), (5, 4), (10, 2), (20, 1)
 console.log(solution(100));
 // 9 => (1, 100), (2, 50), (4, 25), (5, 20), (10, 10), (20, 5), (25, 4), (50, 2), (100, 1)
+
+// case 2
+function solution(n) {
+  return Array(n)
+    .fill(1)
+    .map((v, idx) => v + idx)
+    .filter((v) => n % v === 0).length;
+}
