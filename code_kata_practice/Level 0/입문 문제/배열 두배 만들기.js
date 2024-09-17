9ddf816f7c90fafeafe313b8d8eac87d6b080d5a
dodoyoo <dong15903@daum.net>
2024-09-17 23:07:08 +0900

    feat 2024-09-17

--- code_kata_practice/Level 0/입문 문제/배열 두배 만들기.js
+++ code_kata_practice/Level 0/입문 문제/배열 두배 만들기.js
@@ -13,3 +13,8 @@ function solution(numbers) {
 
 console.log(solution([1, 2, 3, 4, 5])); //[2, 4, 6, 8, 10]
 console.log(solution([1, 2, 100, -99, 1, 2, 3])); // [2, 4, 200, -198, 2, 4, 6]
+
+// case 2
+function solution(numbers) {
+  return numbers.reduce((a, b) => [...a, b * 2], []);
+}
