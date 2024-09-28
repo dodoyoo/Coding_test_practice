153a3c321e43a58f8cd54e18728a5cce2ba8089e
dodoyoo <dong15903@daum.net>
2024-09-28 15:48:06 +0900

    feat 2024-09-28

--- code_kata_practice/Level 0/입문 문제/n의 배수 고르기.js
+++ code_kata_practice/Level 0/입문 문제/n의 배수 고르기.js
@@ -20,3 +20,8 @@ function solution(n, numlist) {
 console.log(solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12])); // [6, 9, 12]
 console.log(solution(5, [1, 9, 3, 10, 13, 5])); // [10, 5]
 console.log(solution(12, [2, 100, 120, 600, 12, 12])); // [120, 600, 12, 12]
+
+//case 2
+function solution(n, numlist) {
+  return numlist.filter((num) => num % n === 0);
+}
