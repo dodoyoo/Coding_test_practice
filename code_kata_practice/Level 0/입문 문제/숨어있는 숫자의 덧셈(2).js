26e9fd3156f1c1455444dc5936f01ff06314b406
dodoyoo <dong15903@daum.net>
2024-12-14 22:55:54 +0900

    feat 2024-12-14

---
+++ code_kata_practice/Level 0/입문 문제/숨어있는 숫자의 덧셈(2).js
@@ -0,0 +1,8 @@
+/*
+문자열 my_string이 매개변수로 주어집니다.
+my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. 
+my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+function solution(my_string) {
+  return my_string.split(/[a-zA-z]/g).reduce((a, c) => +a + +c, 0);
+}
