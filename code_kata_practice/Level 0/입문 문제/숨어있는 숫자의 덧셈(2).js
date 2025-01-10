9601921620929c92fec82dcb643b109109431551
dodoyoo <dong15903@daum.net>
2025-01-10 18:15:00 +0900

    feat 2025-01-10

--- code_kata_practice/Level 0/입문 문제/숨어있는 숫자의 덧셈(2).js
+++ code_kata_practice/Level 0/입문 문제/숨어있는 숫자의 덧셈(2).js
@@ -6,3 +6,16 @@ my_string안의 자연수들의 합을 return하도록 solution 함수를 완성
 function solution(my_string) {
   return my_string.split(/[a-zA-z]/g).reduce((a, c) => +a + +c, 0);
 }
+
+// case 2
+function solution(n) {
+  let answer = '';
+  for (let i = 0; i < n.length; i++) {
+    if (Number.isInteger(+n[i])) {
+      answer += n[i];
+    } else {
+      answer += ' ';
+    }
+  }
+  return answer.split(' ').reduce((acc, cur) => (acc += +cur), 0);
+}
