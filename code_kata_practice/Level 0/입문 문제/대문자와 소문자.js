7710c68eab5287f9730ca8584b595ffa5450051d
dodoyoo <dong15903@daum.net>
2025-01-14 10:00:00 +0900

    feat 2025-01-14

--- code_kata_practice/Level 0/입문 문제/대문자와 소문자.js
+++ code_kata_practice/Level 0/입문 문제/대문자와 소문자.js
@@ -15,3 +15,16 @@ function solution(my_string) {
 
 console.log(solution('cccCCC')); // “CCCccc”
 console.log(solution('abCdEfghIJ')); //"ABcDeFGHij"
+
+// case 2
+function solution(my_string) {
+  let answer = '';
+  for (let i of my_string) {
+    if (i === i.toUpperCase()) {
+      answer += i.toLowerCase();
+    } else {
+      answer += i.toUpperCase();
+    }
+  }
+  return answer;
+}
