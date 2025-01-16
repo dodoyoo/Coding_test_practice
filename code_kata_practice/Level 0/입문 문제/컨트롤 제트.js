5b2794ca7f14d92cf994552f364b68d332f42284
dodoyoo <dong15903@daum.net>
2025-01-16 13:59:21 +0900

    feat 2025-01-16

--- code_kata_practice/Level 0/입문 문제/컨트롤 제트.js
+++ code_kata_practice/Level 0/입문 문제/컨트롤 제트.js
@@ -17,3 +17,15 @@ function solution(s) {
   });
   return answer;
 }
+
+// case 2
+function solution(s) {
+  const stack = [];
+
+  s.split(' ').forEach((target) => {
+    if (target === 'Z') stack.pop();
+    else stack.push(+target);
+  });
+
+  return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
