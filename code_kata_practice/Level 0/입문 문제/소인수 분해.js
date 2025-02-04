0e44ac254ea39935186f88b13ec6f692f8aa8614
dodoyoo <dong15903@daum.net>
2025-02-04 16:47:00 +0900

    feat 2025-02-04

--- code_kata_practice/Level 0/입문 문제/소인수 분해.js
+++ code_kata_practice/Level 0/입문 문제/소인수 분해.js
@@ -20,3 +20,20 @@ function solution(n) {
 
   return [...new Set(result)];
 }
+
+// case 2
+function solution(n) {
+  let answer = [];
+
+  let i = 2;
+  while (i <= n) {
+    if (n % i === 0) {
+      answer.push(i);
+      n = n / i;
+    } else {
+      i++;
+    }
+  }
+
+  return [...new Set(answer.sort((a, b) => (a > b ? 1 : -1)))];
+}
